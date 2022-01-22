b7119af69ce0ab3a5c03e7cf58293b1fb259828c
AdhirajSingh1311 <82698616+AdhirajSingh1311@users.noreply.github.com>
2022-01-22 11:54:44 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCCkvDQx6Y_7mSqOLgx2zNCqLqSz5iWqfA",
+    authDomain: "story-telling-app-89d33.firebaseapp.com",
+    databaseURL: "https://story-telling-app-89d33-default-rtdb.firebaseio.com",
+    projectId: "story-telling-app-89d33",
+    storageBucket: "story-telling-app-89d33.appspot.com",
+    messagingSenderId: "411987103998",
+    appId: "1:411987103998:web:ec63bb652c772ba51195af"
+  };
+  firebase.initializeApp(firebaseConfig)
